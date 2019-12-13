e0d5215b4f4bf5f00545aec22a6d9ff2dded3697
Kounah <kounalyn@gmail.com>
2019-12-13 16:34:23 +0100

    trying to make jsdoc show tips for the functions

--- fn/index.js
+++ fn/index.js
@@ -1,7 +1,20 @@
+/**@type {import('./defined')} */
 module.exports.defined = require('./defined');
+
+/**@type {import('./defined')} */
 module.exports.definedThrow = require('./defined-throw');
+
+/**@type {import('./extend')} */
 module.exports.extend = require('./extend');
+
+/**@type {import('./check')} */
 module.exports.check = require('./check');
+
+/**@type {import('./check-throw')} */
 module.exports.checkThrow = require('./check-throw');
+
+/**@type {import('./check-object')} */
 module.exports.checkObject = require('./check-object');
+
+/**@type {import('./ensure-type')} */
 module.exports.ensureType = require('./ensure-type');
