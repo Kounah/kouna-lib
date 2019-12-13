0d552785ebd748bb40faf8ffd5a9e7b3c3925730
Kounah <kounalyn@gmail.com>
2019-12-13 17:07:18 +0100

    might work

--- fn/index.js
+++ fn/index.js
@@ -1,9 +1,16 @@
+/**@type {import('./defined').DefinedFunction} */
 const defined = require('./defined');
+/**@type {import('./defined-throw').DefinedThrowFunction} */
 const definedThrow = require('./defined-throw');
+/**@type {import('./extend').ExtendFunction} */
 const extend = require('./extend');
+/**@type {import('./check').CheckFunction} */
 const check = require('./check');
+/**@type {import('./check-throw').CheckThrowFunction} */
 const checkThrow = require('./check-throw');
+/**@type {import('./check-object').CheckObjectFunction} */
 const checkObject = require('./check-object');
+/**@type {import('./ensure-type').EnsureTypeFunction} */
 const ensureType = require('./ensure-type');
 
 module.exports = { 
